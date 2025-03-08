2273a314142dd9ea3d4c24969ab783c73510fd61
WADE <MEANTOINE12@GMAIL.COM>
2025-03-08 21:03:38 +0300

    complete

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(street){
+    return Math.abs(street - 42)
+
+}
+
+function distanceFromHqInFeet(street){
+    const distanceInFeet = distanceFromHqInBlocks(street) * 264
+    return distanceInFeet
+}
+
+function distanceTravelledInFeet(start,destination){
+    return Math.abs(start-destination) * 264
+}
+function calculatesFarePrice(start, destination){
+    let distanceTravelled =  Math.abs(start-destination) * 264
+    if(distanceTravelled < 400){
+        return 0
+    }else if(distanceTravelled >= 400 && distanceTravelled <= 2000 ){
+        return (distanceTravelled - 400) * 0.02
+
+    }else if(distanceTravelled > 2000 && distanceTravelled <=2500 ){
+        return 25
+    }else if (distanceTravelled > 2500){
+        return "cannot travel that far"
+    }
+
+}
